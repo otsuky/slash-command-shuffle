828789166b08a46d988b0d2dd5d8c7f5f0f2ccd3
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2020-01-12 01:27:26 +0900

    変数整理

--- src/_modules/createResp.js
+++ src/_modules/createResp.js
@@ -1,14 +1,13 @@
-const createResp = (array) => {
-
+const createResp = array => {
   let result = "";
 
-  for (let i = 0; i < randomArray.length; i++) {
-    result += `${i + 1}: ${randomArray[i]}\n`
+  for (let i = 0; i < array.length; i++) {
+    result += `${i + 1}: ${array[i]}\n`
   }
 
   const response = {
     "response_type": "in_channel",
-    "text": "\n\n*【結果】*\n>" + targetString + "\n================\n" + result + "================\n",
+    "text": "\n\n*【結果】*\n>" + "\n================\n" + result + "================\n",
   };
 
   return response;
